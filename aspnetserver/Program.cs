3e0fa9172f8b90e30aef25e16515570abd1c19fb
Hunter Cordon <hccordon@student.fullsail.edu>
2022-02-10 14:19:56 -0500

    Updated swaggerUI

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -1,19 +1,34 @@
+using aspnetserver;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(SwaggerGenOptionsExtensions =>
+{
+    SwaggerGenOptionsExtensions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Bugit Web Api", Version = "v1" });
+});
 
 var app = builder.Build();
 
+
+
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(swaggerUIOptionsrExtensions =>
+{
+
+    swaggerUIOptionsrExtensions.DocumentTitle = "BugIt server API";
+    swaggerUIOptionsrExtensions.SwaggerEndpoint("/swagger/v1/swagger.json", "web API");
+
+});
 
 
 app.UseHttpsRedirection();
 
-
+//app.MapGet("/get-all-users", async () => await Endpoints.GetUsers());
 
 app.Run();
-
