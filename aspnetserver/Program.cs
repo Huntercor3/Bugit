1d70b16bb926c43bae19247b59a8ed389bf27f92
Hunter Cordon <hccordon@student.fullsail.edu>
2022-02-09 19:50:08 -0500

    deployed + added constants to react

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -1,16 +1,40 @@
+
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000", "https://bugitretry.azurewebsites.net");
+        });
+}); 
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(SwaggerGenOptionsExtensions =>
+{
+    SwaggerGenOptionsExtensions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Bugit Web Api", Version = "v1" });
+});
 
 var app = builder.Build();
 
 
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(swaggerUIOptionsrExtensions =>
+{
+
+    swaggerUIOptionsrExtensions.DocumentTitle = "BugIt server API";
+    swaggerUIOptionsrExtensions.SwaggerEndpoint("/swagger/v1/swagger.json", "web API");
+    
+});
 
 
 app.UseHttpsRedirection();
---
+++ reactclient/src/Utilities/Constants.js
@@ -0,0 +1,1 @@
+const API_BASE_URL_PRODUCTION = "https://bugitserver.azurewebsites.net"; //add "/swagger" at end of this to see server UI
