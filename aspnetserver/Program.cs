d122396ce425df7561ed094815dd3e34132a1ca7
Hunter Cordon <hccordon@student.fullsail.edu>
2022-02-10 22:11:41 -0500

    Changed build inpackage.json

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -28,6 +28,7 @@ app.UseSwaggerUI(swaggerUIOptionsrExtensions =>
 
 
 app.UseHttpsRedirection();
+//var person = Endpoints.
 
 //app.MapGet("/get-all-users", async () => await Endpoints.GetUsers());
 
