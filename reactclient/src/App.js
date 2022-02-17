2dec8ef503779bba6868cefc57a527453066f499
Hunter Cordon <hccordon@student.fullsail.edu>
2022-02-16 22:42:54 -0500

    front end registration

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,29 +1,30 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Layout from './components/Sidebar'
-import { Home } from './components/Home'
-import { About } from './components/About'
-import { Contact } from './components/Contact'
-import { NoMatch } from './components/NoMatch'
-import { Component } from 'react'
-import { Login } from './components/Login'
-import { Signup } from './components/Signup'
-import { Account } from './components/Account'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Layout from "./components/Sidebar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { NoMatch } from "./components/NoMatch";
+import { Component } from "react";
+import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
+import { Account } from "./components/Account";
+import Register from "./components/Register";
 
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path='login' element={<Login />} />
-        <Route path='signup' element={<Signup />} />
-        <Route path='/' element={<Layout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="Register" element={<Register />} />
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='account' element={<Account />} />
-          <Route path='*' element={<NoMatch />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="account" element={<Account />} />
+          <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
