6d111faf6930dc6a90f13ed9ec9f9983b68cb9c1
JManness71 <74986019+JManness71@users.noreply.github.com>
2022-03-10 17:47:54 -0500

    Fixed a space in an sql statement

--- aspnetserver/UserDBHelper.cs
+++ aspnetserver/UserDBHelper.cs
@@ -23,7 +23,7 @@ namespace aspnetserver
                 String sql = "INSERT INTO dbo.Users (FirstName, LastName, email, PhoneNumber, Hardware, Role)" +
                     "OUTPUT INSERTED.UserId" +
                     " values ("
-                    + u.firstName + ", " + u.lastName + ", " + u.email + ", " + u.phoneNumber + ", " + u.hardware + ", " + u.role.roleId.ToString() + "," + u.password + ")";
+                    + u.firstName + ", " + u.lastName + ", " + u.email + ", " + u.phoneNumber + ", " + u.hardware + ", " + u.role.roleId.ToString() + ", " + u.password + ")";
 
                 using (SqlCommand command = new SqlCommand(sql, connection))
                 {
