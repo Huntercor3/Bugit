b68d002595fb6055d4a788c9eb6e31690ff5aed8
JonasWalker <67254086+JonasWalker@users.noreply.github.com>
2022-03-14 19:31:34 -0500

    added Table2

--- reactclient/src/components/Home.jsx
+++ reactclient/src/components/Home.jsx
@@ -1,94 +1,99 @@
-import { render } from '@testing-library/react'
+//import { render } from '@testing-library/react'
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 //import BugItLogo from './images/BugItLogo.jpg';
 import './CSS/home.css'
-import Data from './data.json'
-import { Button } from 'react-bootstrap'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator'
 
-function readJson(bugs, index) {
+const data = require('./data.json')
+
+const columns = [
+  {
+    dataField: 'id',
+    text: 'ID',
+    sort: true,
+  },
+  {
+    dataField: 'name',
+    text: 'Name',
+    sort: true,
+  },
+  {
+    dataField: 'date',
+    text: 'Date',
+    sort: true,
+  },
+  {
+    dataField: 'type',
+    text: 'Type',
+    sort: true,
+  },
+  {
+    dataField: 'status',
+    text: 'Status',
+    sort: true,
+  },
+  {
+    dataField: 'priority',
+    text: 'Priority',
+    sort: true,
+  },
+  {
+    dataField: 'estimatedTime',
+    text: 'Estimated Time',
+    sort: true,
+  },
+]
+
+const defaultSorted = [
+  {
+    dataField: 'name',
+    order: 'desc',
+  },
+]
+
+const expandRow = {
+  onlyOneExpanding: true,
+  renderer: (row) => (
+    <div>
+      <p>{`This Expand row is belong to rowKey ${row.id}`}</p>
+      <p>
+        You can render anything here, also you can add additional data on every
+        row object
+      </p>
+      <p>expandRow.renderer callback will pass the origin row object to you</p>
+    </div>
+  ),
+}
+const options = {
+  sizePerPageList: [
+    {
+      text: '5',
+      value: 5,
+    },
+    {
+      text: '10',
+      value: 10,
+    },
+    {
+      text: 'All',
+      value: data.length,
+    },
+  ], // A numeric array is also available. the purpose of above example is custom the text
+}
+
+const Home = (props) => {
   return (
-    <tr key={index}>
-      <td>{bugs.software}</td>
-      <td>{bugs.name}</td>
-      <td>{bugs.date}</td>
-      <td>{bugs.type}</td>
-      <td>{bugs.status}</td>
-      <td>{bugs.priority}</td>
-      <td>{bugs.estimatedTime}</td>
-    </tr>
+    <React.Fragment>
+      <BootstrapTable
+        keyField='id'
+        data={data}
+        columns={columns}
+        expandRow={expandRow}
+        defaultSorted={defaultSorted}
+        pagination={paginationFactory(options)}
+      />
+    </React.Fragment>
   )
 }
-
-function addBug() {
-  console.log('added bug')
-  const newBug = {
-    software: 'BugIt',
-    name: 'Jonas 2',
-    date: 'never',
-    type: 'Optimize',
-    status: 'In Progress',
-    priority: 'Low',
-    estimatedTime: 'trust trust',
-  }
-  document.getElementById('myTable').insertRow(newBug)
-  document.getElementById('myTbody').insertRow(newBug)
-  return
-}
-
-function deleteBug() {
-  console.log('deleted bug')
-  document.getElementById('myTable').deleteRow(1)
-  return
-}
-
-// function showUser() {
-//   const [user, setuser] = useState({ Email: '', Password: '' })
-//   useEffect(() => {
-//     var a = localStorage.getItem('myData')
-//     var b = JSON.parse(a)
-//     console.log(b.Name)
-//     setuser(b)
-//     console.log(user.Name)
-//   }, [])
-//   return (
-//     <>
-//       <div class='bottomleft'>{user.Name}</div>
-//     </>
-//   )
-// }
-
-export const Home = () => (
-  <React.Fragment>
-    <body>
-      <h1>BugIt</h1>
-      <Link to='/createBug'>
-        <Button class='btn btn-success'>New Bug</Button>
-      </Link>
-      <div class='table-responsive'>
-        <table class='table table-striped table-sm' id='myTable'>
-          <thead>
-            <tr>
-              <th>Software</th>
-              <th>Name</th>
-              <th>Date</th>
-              <th>Type</th>
-              <th>Status</th>
-              <th>Priority</th>
-              <th>Estimated Time</th>
-            </tr>
-          </thead>
-          <tbody id='myTbody'>{Data.map(readJson)}</tbody>
-        </table>
-      </div>
-      {/* <div>
-        <button type='button' class='btn btn-success' onClick={addBug}>
-          Add
-        </button>
-        <button type='button' class='btn btn-danger' onClick={deleteBug}>
-          Delete
-        </button>
-      </div> */}
-    </body>
-  </React.Fragment>
-)
+export default Home
---
+++ reactclient/src/components/HunterSucks.jsx
@@ -0,0 +1,94 @@
+import { render } from '@testing-library/react'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+//import BugItLogo from './images/BugItLogo.jpg';
+import './CSS/home.css'
+import Data from './data.json'
+import { Button } from 'react-bootstrap'
+
+function readJson(bugs, index) {
+  return (
+    <tr key={index}>
+      <td>{bugs.software}</td>
+      <td>{bugs.name}</td>
+      <td>{bugs.date}</td>
+      <td>{bugs.type}</td>
+      <td>{bugs.status}</td>
+      <td>{bugs.priority}</td>
+      <td>{bugs.estimatedTime}</td>
+    </tr>
+  )
+}
+
+function addBug() {
+  console.log('added bug')
+  const newBug = {
+    software: 'BugIt',
+    name: 'Jonas 2',
+    date: 'never',
+    type: 'Optimize',
+    status: 'In Progress',
+    priority: 'Low',
+    estimatedTime: 'trust trust',
+  }
+  document.getElementById('myTable').insertRow(newBug)
+  document.getElementById('myTbody').insertRow(newBug)
+  return
+}
+
+function deleteBug() {
+  console.log('deleted bug')
+  document.getElementById('myTable').deleteRow(1)
+  return
+}
+
+// function showUser() {
+//   const [user, setuser] = useState({ Email: '', Password: '' })
+//   useEffect(() => {
+//     var a = localStorage.getItem('myData')
+//     var b = JSON.parse(a)
+//     console.log(b.Name)
+//     setuser(b)
+//     console.log(user.Name)
+//   }, [])
+//   return (
+//     <>
+//       <div class='bottomleft'>{user.Name}</div>
+//     </>
+//   )
+// }
+
+export const Home = () => (
+  <React.Fragment>
+    <body>
+      <h1>BugIt</h1>
+      <Link to='/createBug'>
+        <Button class='btn btn-success'>New Bug</Button>
+      </Link>
+      <div class='table-responsive'>
+        <table class='table table-striped table-sm' id='myTable'>
+          <thead>
+            <tr>
+              <th>Software</th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Type</th>
+              <th>Status</th>
+              <th>Priority</th>
+              <th>Estimated Time</th>
+            </tr>
+          </thead>
+          <tbody id='myTbody'>{Data.map(readJson)}</tbody>
+        </table>
+      </div>
+      {/* <div>
+        <button type='button' class='btn btn-success' onClick={addBug}>
+          Add
+        </button>
+        <button type='button' class='btn btn-danger' onClick={deleteBug}>
+          Delete
+        </button>
+      </div> */}
+    </body>
+  </React.Fragment>
+)
